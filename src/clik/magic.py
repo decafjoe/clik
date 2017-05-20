94c19c2b7886aa6179eaaee80926f5d4b5113f94
Joe Joyce <joe@decafjoe.com>
2017-05-20 15:21:09 -0400

    Adds copyright for code copied from Werkzeug.

--- src/clik/magic.py
+++ src/clik/magic.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-
+"Magic" variables, inspired by Werkzeug's context-local objects.
 
 :author: Joe Joyce <joe@decafjoe.com>
 :copyright: Copyright (c) Joe Joyce, 2009-2017.
@@ -79,6 +79,46 @@ context = Context()
 
 @implements_bool
 class Magic(object):  # pragma: no cover (werkzeug implementation)
+    """
+    Slightly adapted version of Werkzeug's :class:`werkzeug.local.LocalProxy`.
+
+    Original code licensed from the Werkzeug Team under the following terms:
+
+    Copyright (c) 2014 by the Werkzeug Team, see AUTHORS for more details.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+
+        * Redistributions of source code must retain the above copyright
+          notice, this list of conditions and the following disclaimer.
+
+        * Redistributions in binary form must reproduce the above
+          copyright notice, this list of conditions and the following
+          disclaimer in the documentation and/or other materials provided
+          with the distribution.
+
+        * The names of the contributors may not be used to endorse or
+          promote products derived from this software without specific
+          prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    :copyright: Copyright (c) 2014 by the Werkzeug Team
+    :authors: See Werkzeug's AUTHORS file
+    :license: BSD
+    """
+
     def __init__(self, name, context=context):
         context.register(name)
         object.__setattr__(self, '_Magic__name', name)
