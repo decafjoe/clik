156c309edff4776fd6e2ed249aa0dd06d87b9503
Joe Joyce <joe@decafjoe.com>
2017-09-04 13:51:45 -0400

    Fixes lint violations in clik.__init__.
    The first ignore is a catch-all because "# noqa: F401" exceeds the character
    limit for the line.

--- src/clik/__init__.py
+++ src/clik/__init__.py
@@ -2,12 +2,18 @@
 """
 The command line interface kit.
 
+Clik is a tool for writing complex command-line interfaces with minimal
+boilerplate and bookkeeping.
+
+This top-level package pulls together the end user API from the various
+modules within clik.
+
 :author: Joe Joyce <joe@decafjoe.com>
-:copyright: Copyright (c) Joe Joyce, 2009-2017.
+:copyright: Copyright (c) Joe Joyce and contributors, 2009-2017.
 :license: BSD
 """
 __version__ = '0.90.0'
 
 
-from clik.app import app, args, current_app, g, parser, run_children
-from clik.command import catch
+from clik.app import app, args, current_app, g, parser, run_children  # noqa
+from clik.command import catch  # noqa: F401
